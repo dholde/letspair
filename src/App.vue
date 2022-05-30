5e2e62d334068e9db64c2e940b0803de0176327b
Denis Holdeew <hold.denis@gmail.com>
2022-05-30 22:51:58 +0200

    Layout

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,46 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div id="placeholder">Placeholder ...</div>
+  <div class="container">
+    <div id="header" class="header">Header</div>
+    <div id="users" class="user-area">Users</div>
+    <div id="tasks" class="task-area">Tasks</div>
+    <div id="lanes" class="lane-area">Lanes</div>
+  </div>
 </template>
 
 <style>
 @import "@/assets/base.css";
+.container {
+  width: 100vw;
+  height: 100vh;
+  background-color: var(--bg-color-main);
+  display: grid;
+  grid-gap: 0.3em;
+  grid-template-columns: 25% auto;
+  grid-template-rows: 10% 45% 45%;
+  grid-template-areas:
+    "header header"
+    "userarea lanearea"
+    "taskarea lanearea";
+}
+.header {
+  grid-area: header;
+  background-color: var(--bg-color-secondary);
+}
+
+.user-area {
+  grid-area: userarea;
+  background-color: var(--bg-color-secondary);
+}
+
+.task-area {
+  grid-area: taskarea;
+  background-color: var(--bg-color-secondary);
+}
+
+.lane-area {
+  grid-area: lanearea;
+  background-color: var(--bg-color-secondary);
+}
 </style>
