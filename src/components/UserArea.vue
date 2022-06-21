baebb49132de8a00941c625c36c6f7b2829052c4
Denis Holdeew <hold.denis@gmail.com>
2022-06-21 21:33:04 +0200

    Prepare mock response

---
+++ src/components/UserArea.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import { useStore } from "@/stores/letspairStore";
+
+const store = useStore();
+const users = computed(() => {
+  return store.users;
+});
+
+function createUser() {
+  store.createUser();
+}
+</script>
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,12 +1,39 @@
-import { rest } from "msw";
+import type { User } from "@/models/User";
+import { rest, type PathParams, type RestContext } from "msw";
 import { v4 as uuidv4 } from "uuid";
-const task = {
-  id: `${uuidv4()}`,
-  description: "",
-  order: 0,
-};
+
+interface CreateUserRequestBody {
+  id: string;
+  order: number;
+}
+
+interface CreateUserResponse {
+  id: string;
+  order: number;
+}
+
 export const restHandlers = [
   rest.post("http://localhost:3000/task", (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(task));
+    return res(
+      ctx.status(201),
+      ctx.json({
+        id: `${uuidv4()}`,
+        description: "",
+        order: 0,
+      })
+    );
   }),
+  rest.post<CreateUserRequestBody, PathParams<string>, CreateUserResponse>(
+    "http://localhost:3000/user",
+    (req, res, ctx) => {
+      const { order } = req.body;
+      return res(
+        ctx.status(201),
+        ctx.json({
+          id: `${uuidv4()}`,
+          order: order,
+        })
+      );
+    }
+  ),
 ];
