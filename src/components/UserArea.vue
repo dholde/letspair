c1293cf0e5141ff0700b3cbbe5313daa0bec0201
Denis Holdeew <hold.denis@gmail.com>
2022-06-21 21:35:44 +0200

    Templates

--- src/components/UserArea.vue
+++ src/components/UserArea.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { computed } from "vue";
 import { useStore } from "@/stores/letspairStore";
+import PairingUser from "./PairingUser.vue";
 
 const store = useStore();
 const users = computed(() => {
@@ -11,3 +12,18 @@ function createUser() {
   store.createUser();
 }
 </script>
+
+<template>
+  <div id="userArea">
+    Tasks
+    <button @click="createUser">+</button>
+    <PairingUser v-for="user in users" :key="user.id" :user="user" />
+  </div>
+</template>
+
+<style>
+.addButton {
+  height: 20px;
+  width: 20px;
+}
+</style>
