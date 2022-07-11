502342ac9854b3c262b87add36bef60ffcca82b5
Denis Holdeew <hold.denis@gmail.com>
2022-07-11 22:20:50 +0200

    Initial styling for lanes

--- src/components/PairingLane.vue
+++ src/components/PairingLane.vue
@@ -1,4 +1,20 @@
 <script setup lang="ts"></script>
 <template>
-  <div>This is a lane</div>
+  <div class="pairing-lane">
+    <div class="users">These are the users</div>
+    <div class="tasks">These are the tasks</div>
+  </div>
 </template>
+<style scoped>
+.pairing-lane {
+  margin: var(--margin-medium);
+  padding: var(--padding-small);
+  background-color: var(--bg-color-main);
+}
+.users {
+  min-height: 35px;
+}
+.tasks {
+  min-height: 50px;
+}
+</style>
