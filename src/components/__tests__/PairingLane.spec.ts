319b47ac53a775ce20d3e23223b314d4569001c3
Denis Holdeew <hold.denis@gmail.com>
2022-07-15 17:50:43 +0200

    Initial state containing user that should be dropped

--- src/components/__tests__/PairingLane.spec.ts
+++ src/components/__tests__/PairingLane.spec.ts
@@ -3,16 +3,31 @@ import { render, fireEvent } from "@testing-library/vue";
 import { createTestingPinia } from "@pinia/testing";
 import PairingLane from "@/components/PairingLane.vue";
 import { debug } from "console";
+import { v4 as uuidv4 } from "uuid";
 
 describe("PairingLane", () => {
   it("should contain user after dropping user element", async () => {
+    const laneId = uuidv4();
+    const user = {
+      id: uuidv4(),
+      name: "Super Man",
+      laneId: "",
+    };
+    const userId = uuidv4();
     const { findByText, container } = render(PairingLane, {
       global: {
-        plugins: [createTestingPinia({ stubActions: false })],
+        plugins: [
+          createTestingPinia({
+            stubActions: false,
+            initialState: {
+              letsPair: { users: [user] },
+            },
+          }),
+        ],
       },
       props: {
         lane: {
-          id: "123",
+          id: laneId,
         },
       },
     });
@@ -23,7 +38,9 @@ describe("PairingLane", () => {
         dataTransfer: "Data transfer test",
       });
       debug(renderedComponent);
-      await findByText("Data transfer test");
+      await findByText("Super Man");
+    } else {
+      assert.fail("PairingLane component was not rendered.");
     }
   });
 });
