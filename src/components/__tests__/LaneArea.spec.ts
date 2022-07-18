d9b9454fc948995fefeb0ad77a5344dfb87aec0d
Denis Holdeew <hold.denis@gmail.com>
2022-07-18 22:50:05 +0200

    Retrieve user lists from lanes
    TODO: Check for user in lase before and after drop

--- src/components/__tests__/LaneArea.spec.ts
+++ src/components/__tests__/LaneArea.spec.ts
@@ -4,6 +4,7 @@ import LaneArea from "@/components/LaneArea.vue";
 import { createTestingPinia } from "@pinia/testing";
 import { retry } from "./Utils";
 import { v4 as uuidv4 } from "uuid";
+import { debug } from "console";
 
 describe("LaneArea", () => {
   it("Should create a new lanes when clicking on the '+' button", async () => {
@@ -28,24 +29,43 @@ describe("LaneArea", () => {
     // Testing-library's find methods do not work here as there are already listitems in the list before. Meaning findAllByRole will always resolve immediately after first list item was added.
     await retry(assertThree, 1, 1000);
   });
-  it("Should remove user from lane if the same user is dropped on another lane", async () => {
-    const [lane1, lane2] = [{ id: uuidv4() }, { id: uuidv4() }];
-    const user = { id: uuidv4(), name: "John Wayne", laneId: lane1.id };
-    const { getByRole } = render(LaneArea, {
+  it("Should remove user from lane if the same user is moved (dropped) to another lane", async () => {
+    const pairingLane1 = { id: uuidv4() };
+    const pairingLane2 = { id: uuidv4() };
+    const user = { id: uuidv4(), name: "John Wayne", laneId: pairingLane1.id };
+    const { baseElement, getAllByRole } = render(LaneArea, {
       global: {
         plugins: [
           createTestingPinia({
             stubActions: false,
             initialState: {
-              letsPair: { users: [user] },
+              letsPair: { lanes: [pairingLane1, pairingLane2], users: [user] },
             },
           }),
         ],
       },
     });
-    const renderedListOfLanes = getByRole("list");
-    const { findAllByRole } = within(renderedListOfLanes);
-    const renderedLaneListItems = await findAllByRole("listitems");
-    expect(renderedLaneListItems).toHaveLength(2);
+    const renderedLaneArea = baseElement;
+    const elementsWithId = renderedLaneArea.querySelectorAll("li.pairing-lane");
+    console.log(elementsWithId.length);
+    Array.from(elementsWithId).forEach(function (el) {
+      console.log(el.getAttribute("id"));
+    });
+    const renderedListOfLanes = getAllByRole("list");
+    const renderedPairingLane1 = Array.from(
+      renderedLaneArea.querySelectorAll("div.pairing-lane")
+    ).filter(
+      (renderedPairingLane) => renderedPairingLane.id === pairingLane1.id
+    );
+    const renderedPairingLane2 = Array.from(
+      renderedLaneArea.querySelectorAll("div.pairing-lane")
+    ).filter(
+      (renderedPairingLane) => renderedPairingLane.id === pairingLane2.id
+    );
+    const id = renderedLaneArea.querySelectorAll("div.pairing-lane")[0].id;
+    debug;
+    // const { findAllByRole } = within(renderedListOfLanes);
+    // const renderedLaneListItems = await findAllByRole("listitems");
+    // expect(renderedLaneListItems).toHaveLength(2);
   });
 });
