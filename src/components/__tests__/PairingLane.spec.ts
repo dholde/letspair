68b9b864933f67b004e276eb2a5f8e6f32578344
Denis Holdeew <hold.denis@gmail.com>
2022-07-25 08:34:39 +0200

    Test for dropping task into lane

--- src/components/__tests__/PairingLane.spec.ts
+++ src/components/__tests__/PairingLane.spec.ts
@@ -4,9 +4,10 @@ import { createTestingPinia } from "@pinia/testing";
 import PairingLane from "@/components/PairingLane.vue";
 import { debug } from "console";
 import { v4 as uuidv4 } from "uuid";
+import type { Task } from "@/models/Task";
 
 describe("PairingLane", () => {
-  it("should contain user after dropping a user element", async () => {
+  it("should contain user after dropping a PairingUser element", async () => {
     const laneId = uuidv4();
     const userName = "John Wayne";
     const user = {
@@ -46,6 +47,50 @@ describe("PairingLane", () => {
       const userListItems = await findAllByRole("listitem");
       const userListItem = userListItems[0];
       expect(userListItem.innerHTML).toContain(userName);
+    } else {
+      assert.fail("PairingLane component was not rendered.");
+    }
+  });
+
+  it("should contain task after dropping a PairingTask element", async () => {
+    const laneId = uuidv4();
+    const task: Task = {
+      id: uuidv4(),
+      description: "This is an important task",
+      order: 1,
+    };
+    const { findAllByRole, container } = render(PairingLane, {
+      global: {
+        plugins: [
+          createTestingPinia({
+            stubActions: false,
+            initialState: {
+              letsPair: { tasks: [task] },
+            },
+          }),
+        ],
+      },
+      props: {
+        lane: {
+          id: laneId,
+        },
+      },
+    });
+
+    const renderedComponent = container.firstElementChild;
+    if (renderedComponent) {
+      await fireEvent.drop(renderedComponent, {
+        dataTransfer: {
+          getData: (dataType: string) => {
+            if (dataType === "task") {
+              return JSON.stringify(task);
+            }
+          },
+        },
+      });
+      const userListItems = await findAllByRole("listitem");
+      const userListItem = userListItems[0];
+      expect(userListItem.innerHTML).toContain(task.description);
     } else {
       assert.fail("PairingLane component was not rendered.");
     }
