f1f9fef968bef0ff92cb24da696de3f5b791f4b4
Denis Holdeew <hold.denis@gmail.com>
2022-08-05 22:55:16 +0200

    Delete utils which are replaced by composables

--- src/components/PairingTask.vue
+++ src/components/PairingTask.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { computed, ref } from "vue";
-import { addDraftItemToList } from "@/utils/dragAndDropUtils";
 import { useDragAndDrop } from "@/composables/dragAndDrop";
 
 const props = defineProps(["task"]);
