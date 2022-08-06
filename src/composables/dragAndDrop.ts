dad968ffe21df965bd8aede4d3c4fb0d43ab73be
Denis Holdeew <hold.denis@gmail.com>
2022-08-07 00:01:00 +0200

    Composable for dragover event

--- src/composables/dragAndDrop.ts
+++ src/composables/dragAndDrop.ts
@@ -29,6 +29,65 @@ export function useDragEndEvent(target: Ref<HTMLElement | null>) {
   return isDragEnded;
 }
 
+export function useDragOverEvent(
+  draggedItemId: string,
+  target: Ref<HTMLElement | null>
+) {
+  function onDragOver(event: DragEvent) {
+    const userElementDOMRect = target.value?.getBoundingClientRect();
+    const positionYDraggedElement = event.pageY;
+    const draggedElementType = event.dataTransfer?.items[0].type;
+    if (userElementDOMRect && positionYDraggedElement && draggedElementType) {
+      addDraftItemToList(
+        userElementDOMRect,
+        positionYDraggedElement,
+        draggedElementType,
+        draggedItemId
+      );
+    }
+  }
+  useDragEventListener(target, "dragover", onDragOver);
+  function addDraftItemToList(
+    draggedOverElementDOMRect: DOMRect,
+    draggedElementYPosition: number,
+    draggedElementType: string,
+    draggedOverElementId: string
+  ) {
+    const store = useStore();
+    const addAbove =
+      draggedOverElementDOMRect.y + draggedOverElementDOMRect.height / 2 >
+      draggedElementYPosition
+        ? true
+        : false;
+    const addAboveOriginal = store.dragAndDropInfo.addAbove;
+    if (
+      (store.dragAndDropInfo.draggedOverItemId !== draggedOverElementId &&
+        store.dragAndDropInfo.draggedItemId !== draggedOverElementId) ||
+      (store.dragAndDropInfo.draggedItemId !== draggedOverElementId &&
+        addAboveOriginal !== null &&
+        addAboveOriginal !== addAbove)
+    ) {
+      store.dragAndDropInfo.addAbove = addAbove;
+      store.dragAndDropInfo.draggedOverItemId = draggedOverElementId;
+      if (store.dragAndDropInfo.draggedItemId) {
+        if (draggedElementType === "user") {
+          store.addDraftUserToLane(
+            store.dragAndDropInfo.draggedItemId,
+            draggedOverElementId,
+            addAbove
+          );
+        } else if (draggedElementType === "task") {
+          store.addDraftTaskToLane(
+            store.dragAndDropInfo.draggedItemId,
+            draggedOverElementId,
+            addAbove
+          );
+        }
+      }
+    }
+  }
+}
+
 export function useDragAndDrop(
   draggedItemId: string,
   dataTransferType: string,
