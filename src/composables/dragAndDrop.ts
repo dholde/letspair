4963695eca389bf86f403665da8be442bf7fde5d
Denis Holdeew <hold.denis@gmail.com>
2022-08-16 19:08:52 +0200

    Fix import syntax

--- src/composables/dragAndDrop.ts
+++ src/composables/dragAndDrop.ts
@@ -1,5 +1,5 @@
 import { useStore } from "@/stores/letspairStore";
-import { ref, onUnmounted, unref, watch, type Ref } from "vue";
+import type { Ref } from "vue";
 import { useDragEventListener } from "./event";
 export function useDragStartEvent(
   draggedItemId: string,
