d7c91031109396b7c6ac19ca3f1cf1ca08068396
Denis Holdeew <hold.denis@gmail.com>
2022-08-19 17:02:35 +0200

    Cleanup commented code

--- src/components/TaskArea.vue
+++ src/components/TaskArea.vue
@@ -2,8 +2,6 @@
 import { computed, ref } from "vue";
 import { useStore } from "@/stores/letspairStore";
 import PairingTask from "./PairingTask.vue";
-import type { Task } from "@/models/Task";
-import axios from "axios";
 import { useDropEvent } from "@/composables/dragAndDrop";
 const store = useStore();
 const tasks = computed(() => {
@@ -15,31 +13,8 @@ function createTask() {
 }
 
 useDropEvent(taskAreaElement, "http://localhost:3000/task");
-
-// async function onDrop(event: DragEvent) {
-//   event.preventDefault();
-//   const dataTransfer = event.dataTransfer;
-//   if (dataTransfer) {
-//     const dataTransferType = dataTransfer.items[0].type;
-//     if (dataTransferType === "task") {
-//       const taskAsString: string = event.dataTransfer?.getData(
-//         "task"
-//       ) as string;
-//       const taskFromDropEvent = JSON.parse(taskAsString) as Task;
-//       taskFromDropEvent.laneId = undefined;
-//       await axios.put("http://localhost:3000/task", taskFromDropEvent);
-//       store.freeUpTask(taskFromDropEvent);
-//     }
-//   }
-// }
 </script>
 <template>
-  <!-- <div
-    id="taskArea"
-    @dragenter.prevent
-    @dragover.prevent
-    @drop="onDrop($event)"
-  > -->
   <div id="taskArea" @dragenter.prevent @dragover.prevent ref="taskAreaElement">
     Tasks
     <button @click="createTask">+</button>
