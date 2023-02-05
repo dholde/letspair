7fda396e6c57a51251e8e05a7a24f32d1b3a4b51
Denis Holdeew <hold.denis@gmail.com>
2023-02-05 21:43:22 +0100

    WIP PairingTask - Test dragstart on task -> task component needs to be properly set up

--- src/components/__tests__/PairingTask.spec.ts
+++ src/components/__tests__/PairingTask.spec.ts
@@ -2,38 +2,71 @@ import { mount } from "@vue/test-utils";
 import { v4 as uuidv4 } from "uuid";
 import PairingTask from "@/components/PairingTask.vue";
 import type { Task } from "@/models/Task";
+import { render, fireEvent } from "@testing-library/vue";
+import { createTestingPinia } from "@pinia/testing";
+import { useStore } from "@/stores/letspairStore";
+import { nextTick } from "vue";
 
 describe("PairingTask", () => {
-  it("should call onDrag function when start dragging the element", async () => {
+  it("should set 'isDragged' prop to ture when start dragging it", async () => {
     const task: Task = {
       id: uuidv4(),
-      description: "This is an important task",
+      description: "Task 1",
       order: 1,
+      laneId: "",
     };
-    const dataTransferType = "task";
-    const dataTransferData = JSON.stringify(task);
-    const wrapper = mount(PairingTask, {
-      props: {
-        task,
+    const { container } = render(PairingTask, {
+      global: {
+        plugins: [
+          createTestingPinia({
+            stubActions: false,
+            initialState: {
+              letsPair: {
+                tasks: [task],
+              },
+            },
+          }),
+        ],
       },
     });
-    const onDragStart = vi.spyOn(wrapper.vm, "dragStartHandler");
-    await wrapper.trigger("dragstart");
-    expect(onDragStart).toHaveBeenCalledOnce();
-    expect(onDragStart).toBeCalledWith(
-      dataTransferType,
-      dataTransferData,
-      expect.objectContaining({ target: expect.any(HTMLDivElement) })
-    );
-    const taskElement = expect.objectContaining({
-      target: expect.objectContaining({
-        outerHTML: expect.stringContaining(task.description),
-      }),
-    });
-    expect(onDragStart).toBeCalledWith(
-      dataTransferType,
-      dataTransferData,
-      taskElement
-    );
+    const taskComponent = container.firstElementChild;
+    if (taskComponent) {
+      await nextTick();
+      await fireEvent.dragStart(taskComponent);
+      const store = useStore();
+      expect(store.$state.dragAndDropInfo.draggedItemId).toEqual("123");
+    }
   });
+  // it("should call onDrag function when start dragging the element", async () => {
+  //   const task: Task = {
+  //     id: uuidv4(),
+  //     description: "This is an important task",
+  //     order: 1,
+  //   };
+  //   const dataTransferType = "task";
+  //   const dataTransferData = JSON.stringify(task);
+  //   const wrapper = mount(PairingTask, {
+  //     props: {
+  //       task,
+  //     },
+  //   });
+  //   const onDragStart = vi.spyOn(wrapper.vm, "dragStartHandler");
+  //   await wrapper.trigger("dragstart");
+  //   expect(onDragStart).toHaveBeenCalledOnce();
+  //   expect(onDragStart).toBeCalledWith(
+  //     dataTransferType,
+  //     dataTransferData,
+  //     expect.objectContaining({ target: expect.any(HTMLDivElement) })
+  //   );
+  //   const taskElement = expect.objectContaining({
+  //     target: expect.objectContaining({
+  //       outerHTML: expect.stringContaining(task.description),
+  //     }),
+  //   });
+  //   expect(onDragStart).toBeCalledWith(
+  //     dataTransferType,
+  //     dataTransferData,
+  //     taskElement
+  //   );
+  // });
 });
