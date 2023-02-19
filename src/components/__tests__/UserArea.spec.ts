c7d4a6cf14b5f63b01d504005948ef4c8e035b88
Denis Holdeew <hold.denis@gmail.com>
2023-02-19 15:39:57 +0100

    Fix UserArea test only containing users without laneId

--- src/components/__tests__/UserArea.spec.ts
+++ src/components/__tests__/UserArea.spec.ts
@@ -6,50 +6,55 @@ import { createTestingPinia } from "@pinia/testing";
 import { v4 as uuidv4 } from "uuid";
 
 describe("UserArea", () => {
-  it("creates new user when pressing the '+' button", async () => {
-    const { getByRole, findByText } = render(UserArea, {
+  it("creates new user with placeholder text 'Name' when pressing the '+' button", async () => {
+    const { getByRole, findByPlaceholderText } = render(UserArea, {
       global: {
         plugins: [createTestingPinia({ stubActions: false })],
       },
     });
     const createUserButton = getByRole("button", { name: "+" });
     await fireEvent.click(createUserButton);
-    await findByText("User Name");
+    await findByPlaceholderText("Name");
   });
   it("contains only users that are not assigned to any PairingLane", async () => {
     const user1 = { id: uuidv4(), name: "Bruce Wayne", laneId: uuidv4() };
     const user2 = { id: uuidv4(), name: "Peter Parker", laneId: uuidv4() };
     const user3 = { id: uuidv4(), name: "Robert Bruce Banner", laneId: "" };
     const user4 = { id: uuidv4(), name: "Barry Allen" };
-    const { queryByRole, queryByText } = render(UserArea, {
-      global: {
-        plugins: [
-          createTestingPinia({
-            stubActions: false,
-            initialState: {
-              letsPair: {
-                users: [user1, user2, user3, user4],
+    const { container, queryByRole, queryByText, findAllByPlaceholderText } =
+      render(UserArea, {
+        global: {
+          plugins: [
+            createTestingPinia({
+              stubActions: false,
+              initialState: {
+                letsPair: {
+                  users: [user1, user2, user3, user4],
+                },
               },
-            },
-          }),
-        ],
-      },
-    });
-    const userListItems = queryByRole("listitems");
-    expect(userListItems).toBeNull;
-    const elementConatainingUserName1 = queryByText(user1.name);
-    expect(elementConatainingUserName1).toBeNull();
-    const elementConatainingUserName2 = queryByText(user2.name);
-    expect(elementConatainingUserName2).toBeNull();
-    const elementConatainingUserName3 = queryByText(user3.name);
-    expect(elementConatainingUserName3?.innerHTML).toBe(user3.name);
-    const elementConatainingUserName4 = queryByText(user4.name);
-    expect(elementConatainingUserName4?.innerHTML).toBe(user4.name);
+            }),
+          ],
+        },
+      });
+    await nextTick();
+    const pairingUserElements = await findAllByPlaceholderText("Name");
+    const usersExpectedInLane = pairingUserElements.filter(
+      (userInput) =>
+        (userInput as HTMLFormElement).value === user3.name ||
+        (userInput as HTMLFormElement).value === user4.name
+    );
+    expect(usersExpectedInLane).length(2);
+    const userNotExpectedInLane = pairingUserElements.filter(
+      (userInput) =>
+        (userInput as HTMLFormElement).value === user1.name ||
+        (userInput as HTMLFormElement).value === user2.name
+    );
+    expect(userNotExpectedInLane).length(0);
   });
 
   it("contains a user after dropping the user into the UserArea", async () => {
     const user = { id: uuidv4(), name: "John Wayne", laneId: uuidv4() };
-    const { container, queryByText, findByText } = render(UserArea, {
+    const { container, queryByText, findByRole } = render(UserArea, {
       global: {
         plugins: [
           createTestingPinia({
@@ -78,7 +83,14 @@ describe("UserArea", () => {
           items: [{ type: "user" }],
         },
       });
-      await findByText("John Wayne");
+      const pairingUserElement = await findByRole("input");
+      if (pairingUserElement) {
+        expect((pairingUserElement as HTMLFormElement).value).toEqual(
+          "John Wayne"
+        );
+      } else {
+        assert.fail("Cannot find dropped user in lane");
+      }
     } else {
       assert.fail("The UserArea component was not rendered.");
     }
