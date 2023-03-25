995ac2fb9cbf7a9d87b4f439e551b2082e28aa79
Denis Holdeew <hold.denis@gmail.com>
2023-03-25 19:51:03 +0100

    add routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,31 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Letspair");
+});
+
+app.get("/users", (req, res) => {
+  res.send("GET users");
+});
+
+app.get("/users/:id", (req, res) => {
+  res.send("GET user by id");
+});
+
+app.get("/tasks", (req, res) => {
+  res.send("GET tasks");
+});
+
+app.get("/tasks/:id", (req, res) => {
+  res.send("GET task by id");
+});
+
+app.get("/lanes", (req, res) => {
+  res.send("GET lanes");
+});
+
+app.get("/lanes/:id", (req, res) => {
+  res.send("GET lane by id");
 });
 
 app.listen(port, () => {
