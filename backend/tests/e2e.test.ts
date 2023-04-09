2e7dac1553b13848f7551d88e174355da40210c2
Denis Holdeew <hold.denis@gmail.com>
2023-04-09 14:27:45 +0200

    Add done

--- backend/tests/e2e.test.ts
+++ backend/tests/e2e.test.ts
@@ -15,28 +15,31 @@ afterAll(async () => {
 });
 
 describe("Test the root path", () => {
-  test("It should respond to the GET method", async () => {
+  test("It should respond to the GET method", async (done) => {
     const response = await request(app).get("/");
     expect(response.statusCode).toBe(200);
     expect(response.text).toBe("Hello World!");
+    done();
   });
 });
 
 describe("Test the /users path", () => {
-  test("It should respond to the GET method", async () => {
+  test("It should respond to the GET method", async (done) => {
     const response = await request(app).get("/users");
     expect(response.statusCode).toBe(200);
     expect(response.body).toEqual([
       { id: 1, name: "Alice" },
       { id: 2, name: "Bob" },
     ]);
+    done();
   });
 
-  test("It should respond to the POST method", async () => {
+  test("It should respond to the POST method", async (done) => {
     const response = await request(app)
       .post("/users")
       .send({ id: 3, name: "Charlie" });
     expect(response.statusCode).toBe(200);
     expect(response.body).toEqual({ id: 3, name: "Charlie" });
+    done();
   });
 });
